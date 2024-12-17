c1795e3468ea526bda810d5210214023ea112949
Samet Yalçın <106279672+FarzetkiTR38@users.noreply.github.com>
2024-12-17 04:20:37 +0300

    Update Samet Yalçın 2412729004 Algortima Ödev 08.cpp

--- Samet Yalçın 2412729004 Algortima Ödev 08.cpp
+++ Samet Yalçın 2412729004 Algortima Ödev 08.cpp
@@ -14,13 +14,13 @@ int main() {
 setlocale(LC_ALL, "Turkish");
 system("Color 0A");
 
-	//	randmax = rand() % (büyüksayi - küçüksayi + 1) + küçüksayi    // +1 ekleme sebebimiz en büyük sayıyı dahil etmek.
+	//	randmax = rand() % (bÃ¼yÃ¼ksayi - kÃ¼Ã§Ã¼ksayi + 1) + kÃ¼Ã§Ã¼ksayi    // +1 ekleme sebebimiz en bÃ¼yÃ¼k sayÄ±yÄ± dahil etmek.
 	srand(time(0));
 
 	float kesirlisayilar2[500];
 	for (int i = 0; i<500; i++) {
 		kesirlisayilar2[i] = (float)rand() / (float)RAND_MAX;
-		cout << (i+1) << ". üretilen kesirli sayı --> " << kesirlisayilar2[i] << endl; 
+		cout << (i+1) << ". Ã¼retilen kesirli sayÄ± --> " << kesirlisayilar2[i] << endl; 
 	}
 	
 return 0;
